73911aecc3240eee5140ca10caa57dea48a22626
Austin Lawrence <Ablarry91@gmail.com>
2015-03-09 10:47:07 -0500

    Grabbing geometry message properly
    Former-commit-id: 6af88c8a7deb3391d90228c7d0a2ac63de4efa92

--- monocular_pose_estimator/src/pid.py
+++ monocular_pose_estimator/src/pid.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+def callback(data):
+	# you can find more info about a particular topic by typing in 'rosmsg show geometry_msgs/PoseWithCovarianceStamped'
+	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose.pose.position)
+	
+def listener():
+
+	# In ROS, nodes are uniquely named. If two nodes with the same
+	# node are launched, the previous one is kicked off. The
+	# anonymous=True flag means that rospy will choose a unique
+	# name for our 'listener' node so that multiple listeners can
+	# run simultaneously.
+	rospy.init_node('pid', anonymous=True)
+
+	rospy.Subscriber("/monocular_pose_estimator/estimated_pose", PoseWithCovarianceStamped, callback)
+
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+	listener()
