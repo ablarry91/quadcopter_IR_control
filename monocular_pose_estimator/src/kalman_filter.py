120ab4281e4033381616e72a26c7115a82bdffff
Austin Lawrence <Ablarry91@gmail.com>
2015-04-18 15:33:56 -0500

    Structured the code such that I can call a function at the rate of the monocular pose estimator publish rate, as well as another independent publisher which will ultimately be the filter output.

--- monocular_pose_estimator/src/kalman_filter.py
+++ monocular_pose_estimator/src/kalman_filter.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
+
+def filterNode(data):
+
+	pub = rospy.Publisher('filter_output', String, queue_size=10)
+	hello_str = "should be slow %s" % rospy.get_time()
+	rospy.loginfo(hello_str)
+	pub.publish(hello_str)
+	# print "hi"
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+	rospy.init_node('filter_node', anonymous=True)
+	rospy.Subscriber("/monocular_pose_estimator/estimated_pose", PoseWithCovarianceStamped, filterNode)
+	# pub = rospy.Publisher('filter_output', String, queue_size=10)
+
+	while not rospy.is_shutdown():
+		pub = rospy.Publisher('filter_test', String, queue_size=10)
+		hello_str = "should be fast %s" % rospy.get_time()
+		rospy.loginfo(hello_str)
+		pub.publish(hello_str)
+		rate = rospy.Rate(100) # 10hz
+		rate.sleep()
+
+	# hello_str = "hi world %s" % rospy.get_time()
+	# rospy.loginfo(hello_str)
+	# pub.publish(hello_str)
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+	talker()
