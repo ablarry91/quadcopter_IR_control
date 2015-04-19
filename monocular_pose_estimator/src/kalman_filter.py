a0eb6b85c596632504e90eba9e6c598be8b5307a
Austin Lawrence <Ablarry91@gmail.com>
2015-04-18 20:09:33 -0500

    velocity filter compiles properly for a 7 dimensional problem.  Looks to be accurate upon viewing graphs, but will look into it more.

--- monocular_pose_estimator/src/kalman_filter.py
+++ monocular_pose_estimator/src/kalman_filter.py
@@ -26,6 +26,82 @@ def kalmanVel():
 	rospy.Subscriber("/monocular_pose_estimator/estimated_pose", PoseWithCovarianceStamped, poseUpdate)
 	rate = rospy.Rate(100) # 10hz
 
+	#initializers for the kalman filter, representing 7 parameters, first 7 being positions, next 7 being velocities
+	# posEst = np.transpose(np.matrix([0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
+	posEst = np.zeros([14,1])
+	cov = np.identity(14)  #just a guess
+	q=.01
+	Q = np.zeros([14,14])
+	np.fill_diagonal(Q,q)
+	# Q = np.matrix([[q,0,0,0],[0,q,0,0],[0,0,q,0],[0,0,0,q]])
+	r=100
+	R = np.zeros([7,7])
+	np.fill_diagonal(R,r)
+	# R = np.matrix([[r,0],[0,r]])
+
+	#constants
+	f = np.identity(7)
+	F = np.identity(14)
+	for i in range(len(f)):
+		for j in range(len(f)):
+			F[i+7,j+7] = f[i,j]
+	h = np.identity(7)
+	H = np.zeros([7,14])
+	for i in range(len(h)):
+		for j in range(len(h)):
+			H[i,j] = h[i,j]
+
+
+	# F = np.matrix([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]])  #constant vel
+	# H = np.matrix([[1,0,0,0],[0,1,0,0]])
+	
+	#keep looping until all hell breaks loose
+	while not rospy.is_shutdown():
+
+		#state prediction
+		statePred = F*posEst
+		statePredCov = F*cov*np.transpose(F)+Q
+
+		#measurement prediction
+		measPred = np.dot(H,statePred)
+		measPredCov = np.dot(H,statePredCov)
+		measPredCov = np.dot(H,np.transpose(H)) + R	
+
+		#associate the data together
+		measurement = np.matrix([currentPos[0],currentPos[1],currentPos[2],currentPos[3],currentPos[4],currentPos[5],currentPos[6]])
+		measurement = np.transpose(measurement)
+		innovation = measurement-measPred
+
+		#update
+		gain = np.dot(statePredCov,np.transpose(H))
+		gain = np.dot(gain,np.linalg.inv(measPredCov))
+		# gain = statePredCov*np.transpose(H)*np.linalg.inv(measPredCov)
+		posEst = statePred+gain*innovation
+		cov = np.identity(14) - np.dot(gain,H)
+		cov = np.dot(cov,statePredCov)
+		# cov = (np.identity(14)-gain*H)*statePredCov
+
+		#get your final result
+		estimation = np.transpose(posEst)
+		# print "estimation is", estimation
+
+		pub = rospy.Publisher('filter_output', Float32, queue_size=10)
+		test = Float32()
+		test.data = estimation[0,0]
+		# test.data = 5
+		pub.publish(test)
+		# hello_str = "should be fast %s" % rospy.get_time()
+		# rospy.loginfo(hello_str)
+		# pub.publish(hello_str)
+
+		#wait at prescribed frequency
+		rate.sleep()
+
+def kalmanAccel():
+	rospy.init_node('filter_node', anonymous=True)
+	rospy.Subscriber("/monocular_pose_estimator/estimated_pose", PoseWithCovarianceStamped, poseUpdate)
+	rate = rospy.Rate(100) # 10hz
+
 	#initializers for the kalman filter
 	posEst = np.transpose(np.matrix([0,50,10,10]))
 	cov = np.identity(4)  #just a guess
